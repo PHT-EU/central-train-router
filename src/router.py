6fc12aa3aff0d74d5a1d613ea7af744c82e36358
migraf <michael.m.graf@gmx.de>
2021-03-15 22:20:25 +0100

    moving images between harbor projects

--- src/router.py
+++ src/router.py
@@ -27,21 +27,52 @@ class TrainRouter:
         self.harbor_headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
         self.harbor_auth = (self.harbor_user, self.harbor_pw)
 
+    def scan_harbor_project(self, project_id: str):
+        """
+        Scan a harbor projects listing all the repositories in the image
+
+        :param project_id: identifier of the project to scan
+        :return:
+        """
+        url = self.harbor_api + f"/projects/{project_id}/repositories"
+        r = requests.get(url, headers=self.harbor_headers, auth=self.harbor_auth)
+        repos = r.json()
+
+        for repo in repos:
+            train_id = repo["name"].split("/")[-1]
+            if self._check_artifact_label(project_id, train_id):
+                self.process_train(train_id)
+
     def process_train(self, train_id: str):
+        """
+        Processes a train image tagged with the pht_next label according the route stored in redis
+
+        :param train_id:
+        :return:
+        """
+
         route_type = self.redis.get(f"{train_id}-type")
         # TODO perform different actions based on route type
+        print(train_id, route_type)
 
         # If the route exists move to next station project
 
-        if self.redis.get(f"{train_id}-route"):
+        if self.redis.exists(f"{train_id}-route"):
             next_station_id = self.redis.rpop(f"{train_id}-route")
-
+            print(next_station_id)
         # otherwise move to pht_outgoing
         else:
+            # TODO delete route from vault and redis
             pass
 
-
     def sync_routes_with_vault(self):
+        """
+        Gets all routes stored in vault and compares them with the ones stored in redis, if a route does not exist in
+        redis it will be added.
+
+        :return:
+        """
+
         routes = self._get_all_routes_from_vault()
 
         # Iterate over all routes and add them to redis if they dont exist
@@ -82,8 +113,6 @@ class TrainRouter:
         self.redis.set(f"{train_id}-type", "periodic" if route["periodic"] else "linear")
         # TODO store the number of epochs somewhere/ also needs to be set when specifying periodic routes
 
-
-
     def get_route_data_from_vault(self, train_id: str) -> dict:
         """
         Get the route data for the given train_id from the vault REST api
@@ -96,43 +125,39 @@ class TrainRouter:
 
         return r.json()["data"]["data"]
 
-    def _move_image(self, train_id: str, origin: str, dest: str):
+    def _move_train(self, train_id: str, origin: str, dest: str, delete=True):
         # TODO move base and latest image
-        pass
+        url = f"{self.harbor_api}/projects/{dest}/repositories/{train_id}/artifacts"
+        params_latest = {"from": f"{origin}/{train_id}:latest"}
+        params_base = {"from": f"{origin}/{train_id}:base"}
 
-    def scan_harbor(self):
-        pass
+        # Move base image
+        base_r = requests.post(url=url, headers=self.harbor_headers, auth=self.harbor_auth, params=params_base)
+        print(base_r.text)
 
-    def scan_harbor_project(self, project_id: str) -> List[dict]:
-        """
-        Scan a harbor projects listing all the repositories in the image
+        # Move latest image
+        latest_r = requests.post(url=url, headers=self.harbor_headers, auth=self.harbor_auth, params=params_latest)
+        print(latest_r.text)
+        if delete:
+            delete_url = f"{self.harbor_api}/projects/{origin}/repositories/{train_id}/artifacts/"
+            r_latest = requests.delete(delete_url + "latest")
+            print(r_latest.text)
+            r_base = requests.delete(delete_url + "base")
 
-        :param project_id: identifier of the project to scan
-        :return:
-        """
-        url = self.harbor_api + f"/projects/{project_id}/repositories"
-        r = requests.get(url, headers=self.harbor_headers, auth=self.harbor_auth)
-        repos = r.json()
 
-        for repo in repos:
-            train_id = repo["name"].split("/")[-1]
-            if self._check_artifact_label(project_id, train_id):
-                # TODO get route and move image
-                pass
-        return repos
+
+
+
 
     def _check_artifact_label(self, project_id: str, train_id: str, tag: str = "latest"):
         url = f'{self.harbor_api}/projects/{project_id}/repositories/{train_id}/artifacts/{tag}'
         r = requests.get(url=url, headers=self.harbor_headers, auth=self.harbor_auth, params={"with_label": True})
         labels = r.json()["labels"]
         if labels and not any(d["name"] == "pht_next" for d in labels):
-            print("Found next label ")
+            print("Found next label")
             return True
         else:
             return False
-
-
-
 
 
 if __name__ == '__main__':
@@ -142,8 +167,9 @@ if __name__ == '__main__':
     # tr.redis.lpush("route-1", *[1,2,3])
     # print(tr.redis.lrange("route-1", 0, -1))
 
-    train_id = "00673752-7126-4a84-8d25-99fd39fb273d"
+    train_id = "a3ea25fd-8a66-4f00-b193-ac8d5745f44c"
 
     # print(tr.get_route_from_vault(train_id))
-    print(tr.scan_harbor_project("1"))
+    # print(tr.scan_harbor_project("1"))
     # tr.sync_routes_with_vault()
+    tr._move_train(train_id=train_id, origin="1", dest="2")
